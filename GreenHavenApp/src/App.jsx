d58689b3a98ff9392d5e4dcf5ebce66d9a7e3448
Isaac Cyrman <Isaaccyrman@ufm.edu>
2024-12-26 01:40:03 -0600

    Initial Configuration and Progress for Landing Page

---
+++ GreenHavenApp/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+import "./AboutUs"
+import AboutUs from './AboutUs';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+      <div className="background-image"></div>
+
+        <AboutUs></AboutUs>
+
+      </div>
+    </> 
+
+  )
+}
+
+export default App
